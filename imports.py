546f6132081efb9efd8fa8cbef910e4a085d64c3
Augustinas Naina <47611976+augustinasn@users.noreply.github.com>
2021-01-16 09:57:24 +0200

    Update imports.py

--- imports.py
+++ imports.py
@@ -7,6 +7,7 @@ import pickle
 import math
 import scipy
 
+from sklearn.metrics import roc_auc_score
 from IPython.display import display, HTML
 from scipy.cluster import hierarchy as hc
 from pdpbox import pdp
